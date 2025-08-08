72d08c6ac637dee779154471f1aaf6d20ad6bb41
Ali <harleyouharley@gmail.com>
2025-08-08 22:28:53 +0100

    added more pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
 
 export default function App() {
   return (
@@ -9,6 +12,13 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/" element={<Category />} />
+          <Route path="/" element={<Category />} />
+          <Route path="/" element={<Category />} />
+          <Route path="/" element={<Product />}>
+            <Route path=":productId"  element={<Product />}/>
+          </Route>
+          <Route path="/" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
+import { MdHomeFilled, MdCategory, MdShop2, MdContacts } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 
-
-const Navbar = () => {
+const Navbar = ({ containerStyles }) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className={`${containerStyles}`}>
+        <NavLink to="/" className="flexCenter gap-x-1"><MdHomeFilled />Home</NavLink>
+        <NavLink to="/mens" className="flexCenter gap-x-1"><MdCategory />Men's </NavLink>
+        <NavLink to="/womens" className="flexCenter gap-x-1"><MdShop2 />Women's</NavLink>
+        <NavLink to="/kids" className="flexCenter gap-x-1"><MdContacts />Kid's</NavLink>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
