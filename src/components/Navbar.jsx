db7c4c37ebda5883114680d819ec768d3c523002
Ali <harleyouharley@gmail.com>
2025-08-09 20:32:16 +0100

    completing navabar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = ({ containerStyles }) => {
   return (
     <nav className={`${containerStyles}`}>
-        <NavLink to="/" className="flexCenter gap-x-1"><MdHomeFilled />Home</NavLink>
-        <NavLink to="/mens" className="flexCenter gap-x-1"><MdCategory />Men's </NavLink>
-        <NavLink to="/womens" className="flexCenter gap-x-1"><MdShop2 />Women's</NavLink>
-        <NavLink to="/kids" className="flexCenter gap-x-1"><MdContacts />Kid's</NavLink>
+        <NavLink to="/" ><div className="flexCenter gap-x-1"><MdHomeFilled />Home</div></NavLink>
+        <NavLink to="/mens"><div className="flexCenter gap-x-1"><MdCategory />Men's</div></NavLink>
+        <NavLink to="/womens"><div className="flexCenter gap-x-1"><MdShop2 />Women's</div></NavLink>
+        <NavLink to="/kids"><div className="flexCenter gap-x-1"><MdContacts />Kid's</div></NavLink>
     </nav>
   );
 };
