6df035ac54122b9373ba2306d42026b9c076b98e
Ali <harleyouharley@gmail.com>
2025-08-14 20:40:01 +0100

    Home-Hero Mod

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import { MdStar, MdOutlineLocalOffer } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+
 
 const Hero = () => {
   return (
     <section>
+      <div>
+        <h1>Digital Shopping Hub</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          officia dolore voluptatem, sapiente delectus dicta itaque assumenda
+          veritatis nostrum deserunt voluptatibus eum commodi nobis, asperiores,
+          ipsa sed numquam aspernatur nesciunt incidunt dolorum quaerat!
+        </p>
         <div>
-            <h1>Digital Shopping Hub</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus officia dolore voluptatem, sapiente delectus dicta itaque assumenda veritatis nostrum deserunt voluptatibus eum commodi nobis, asperiores, ipsa sed numquam aspernatur nesciunt incidunt dolorum quaerat!</p>
+          <div>
+            <MdStar />
+            <MdStar />
+            <MdStar />
+            <MdStar />
+          </div>
+          <div className="bold-16 sm:bold-20">
+            176K{" "}
+            <span className="regular-16 sm:regular-20">Excellent Reviews</span>
+          </div>
         </div>
+        <div className=''>
+            <NavLink to={''} className={"btn_dark_rounded flexCenter"}>Shop Now</NavLink>
+            <NavLink to={''} className={"btn_dark_rounded flexCenter gap-x-2"}><MdOutlineLocalOffer className="text-2xl"/>Offer</NavLink>
+        </div>
+      </div>
     </section>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Hero from '../components/Hero' // Adjust path based on your folder structure
 
 const Home = () => {
   return (
