85200cee24e8ec960b118d9468183e382eb63923
Ali <harleyouharley@gmail.com>
2025-08-14 20:20:54 +0100

    added Hero.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-transparent ring-1 ring-slate-900/5 z-10">
+    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10">
         <div className="px-4 flexBetween py-3 max-xs:px-2">
             {/* Logo */}
             <div>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <div>
+            <h1>Digital Shopping Hub</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus officia dolore voluptatem, sapiente delectus dicta itaque assumenda veritatis nostrum deserunt voluptatibus eum commodi nobis, asperiores, ipsa sed numquam aspernatur nesciunt incidunt dolorum quaerat!</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
