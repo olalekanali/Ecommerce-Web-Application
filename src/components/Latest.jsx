d950b31e141d5f611b59aa5f35b150e3322dd17c
Ali <harleyouharley@gmail.com>
2025-08-16 11:24:35 +0100

    product section

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,4 +1,4 @@
-import POPULAR from "../assets/popular"
+import LATEST from "../assets/latest"
 import Item from "./Item"
 
 
@@ -6,11 +6,11 @@ const Popular = () => {
   return (
     <section className="bg-primary">
         <div className="max_padd_container py-12 xl:py-28 xl:w-[88%]">
-            <h3 className="h3 text-center">Popular Products</h3>
+            <h3 className="h3 text-center">Latest Products</h3>
             <hr className="h-[3px] md:w-1/2 mx-auto bg-gradient-to-l from-transparent via-black to-transparent mb-16" />
             {/* Container */}
             <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
-              {POPULAR.map((item) =>(
+              {LATEST.map((item) =>(
                 <Item key={item.id} name={item.name} id={item.id} image={item.image} new_price={item.new_price} old_price={item.old_price} /> 
               ))}
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero' 
 import Popular from '../components/Popular'
+import Latest from '../components/Latest'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Popular />
+      <Latest />
     </>
   )
 }
