29e8cc542f99d336e3a106996906b9b99006554d
Ali <harleyouharley@gmail.com>
2025-08-16 08:56:14 +0100

    starting footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,35 @@
-
+import { Link } from "react-router-dom";
+import { FOOTER_LINKS } from "../assets/data";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div>
+        <div>
+          <div>
+            <Link to="/" className="mb-10 bold-20">
+              Shopper
+            </Link>
+          </div>
+          <div>
+            {FOOTER_LINKS.map((col) => (
+              <FooterColumn ></FooterColumn>
+            ))}
+              
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+const FooterColumn = ({ title, children }) => {
+  return (
+    <div>
+      <h4>{title}</h4>
+      {children}
+    </div>
   )
 }
 
-export default Footer
+export default Footer;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,20 @@
-
 import { NavLink } from "react-router-dom";
 import { MdOutlineLocalOffer } from "react-icons/md";
 import { FaStar } from "react-icons/fa";
 
-
 const Hero = () => {
   return (
-    <section className='relative bg-hero bg-[center_top_25%] bg-no-repeat h-screen w-full'>
-      <div className='max_padd_container relative top-32 xs:top-52'>
-        <h1 className='h1 capitalize max-w-[37rem]'>Digital Shopping Hub Junction</h1>
-        <p className='text-gray-50 regular-16 mt-6 max-w-[37rem]'>
+    <section className="relative bg-hero bg-center bg-no-repeat min-h-screen w-full">
+      <div className="max_padd_container relative pt-40 sm:pt-40">
+        <h1 className="h1 capitalize max-w-[37rem]">Digital Shopping Hub Junction</h1>
+        <p className="text-gray-50 regular-16 mt-6 max-w-[37rem]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
           officia dolore voluptatem, sapiente delectus dicta itaque assumenda
           veritatis nostrum deserunt voluptatibus eum commodi nobis, asperiores,
           ipsa sed numquam aspernatur nesciunt incidunt dolorum quaerat!
         </p>
-        <div className='flexStart items-center gap-x-4 my-10'>
-          <div className='regular-24 flexCenter gap-x-1'>
+        <div className="flexStart items-center gap-x-4 my-10">
+          <div className="regular-24 flexCenter gap-x-1">
             <FaStar />
             <FaStar />
             <FaStar />
@@ -27,13 +25,18 @@ const Hero = () => {
             <span className="regular-16 sm:regular-20">Excellent Reviews</span>
           </div>
         </div>
-        <div className='max-xs:flex-col flex gap-2'>
-            <NavLink to={''} className={"btn_dark_rounded flexCenter"}>Shop Now</NavLink>
-            <NavLink to={''} className={"btn_dark_rounded flexCenter gap-x-2"}><MdOutlineLocalOffer className="text-2xl"/>Offer</NavLink>
+        <div className="max-xs:flex-col flex gap-2">
+          <NavLink to={""} className="btn_dark_rounded flexCenter">
+            Shop Now
+          </NavLink>
+          <NavLink to={""} className="btn_dark_rounded flexCenter gap-x-2">
+            <MdOutlineLocalOffer className="text-2xl" />
+            Offer
+          </NavLink>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
