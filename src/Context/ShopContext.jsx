3b4431403c9d53e42f69e4d4b309798dde46c3bb
Ali <harleyouharley@gmail.com>
2025-08-17 07:13:07 +0100

    category creation

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+import all_prodducts from "../assets/all_products"
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+    const contextValue = {all_prodducts}
+}
